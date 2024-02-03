ca7ba08973e5a5a92fd773aed2142099ca464855
Lincolntex <chasen.m.young@gmail.com>
2024-02-02 18:46:13 -0800

    added component lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { Outlet, useNavigate } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,8 +31,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <button onClick={() => { navigate( "about") }}>About</button>
-      <button onClick={() => { navigate( "home") }}>Home</button>
+      <Button onClick={() => { navigate( "about") }} variant='primary'>About</Button>
+      <Button onClick={() => { navigate( "home" ) }} variant='success'>Home</Button>
       <Outlet/>
     </>
   )
