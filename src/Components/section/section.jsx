b1709efabcb08657607b56e1daf67df00b151ba6
Lincolntex <chasen.m.young@gmail.com>
2024-02-14 20:31:08 -0800

    made section component dynamic

--- src/Components/section/section.jsx
+++ src/Components/section/section.jsx
@@ -1,15 +1,13 @@
 import './section.css'
 
-export function Section() {
+export function Section(props) {
     return (
         <div className='section'>
-            <h1 className='heading'>Heading</h1>
+            <h1 className='heading'>{props.title}</h1>
             <div className='content-area'>
                 <div className='left-content'></div>
                 <div className='right-content'>
-                    <h2>University of Nevada Reno</h2>
-                    <p>Bachlors of Science in Information Systems</p>
-                    <p>May 17, 2024</p>
+                    {props.content}
                 </div>
             </div>
             <div className='section-break'></div>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,14 @@
 import { Section } from "../../Components/section/section"
 
 export function Home() {
+    const schoolInfo = <>
+        <h2>University of Nevada Reno</h2>
+        <p>Bachlors of Science in Information Systems</p>
+        <p>May 17, 2024</p>
+        </>
     return(
         <>
-            <Section/>
-            <Section/>
-            <Section/>
+            <Section title='Education' content={schoolInfo}/>
         </>
     )
         
