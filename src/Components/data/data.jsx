31f0345c69a2e593b389e66c40a80b4d1d73cf22
Lincolntex <chasen.m.young@gmail.com>
2024-02-16 19:48:38 -0800

    created Card component and set up data file and dynamic dispay. Fixes #7

---
+++ src/Components/data/data.jsx
@@ -0,0 +1,46 @@
+//export a varibale that is the array containg the object data
+
+export const EDUCATION_CARD_DATA = [
+    { 
+        heading: "University of Nevada Reno", 
+        subHeading: null, 
+        content: "Bachlors of Science in Information Systems", 
+        footer: "May 17, 2024"
+    }
+]
+
+export const WORK_CARD_DATA = [
+    { 
+        heading: "Clear Capital", 
+        subHeading: "Customer Service Rep 1", 
+        content: "description of job and what I did there and stuff yadda yadda yaddya yadda hdahhha", 
+        footer: "Start Date - End Date"
+    },
+    { heading: "Clear Capital", 
+        subHeading: "Customer Service Rep 1", 
+        content: "description of job and what I did there and stuff yadda yadda yaddya yadda hdahhha", 
+        footer: "Start Date - End Date"
+    },
+    { heading: "Clear Capital", 
+        subHeading: "Customer Service Rep 1", 
+        content: "description of job and what I did there and stuff yadda yadda yaddya yadda hdahhha", 
+        footer: "Start Date - End Date"
+    }
+]
+export const PROJECT_CARD_DATA = [
+    { heading: "Name of Project", 
+        subHeading: "Project Link", 
+        content: "description of Project and what it was used for and stuff yadda yadda yaddya yadda hdahhha", 
+        footer: "Start Date - End Date"
+    },
+    { heading: "Clear Capital", 
+        subHeading: "Customer Service Rep 1", 
+        content: "description of job and what I did there and stuff yadda yadda yaddya yadda hdahhha", 
+        footer: "Start Date - End Date"
+    },
+    { heading: "Clear Capital", 
+        subHeading: "Customer Service Rep 1", 
+        content: "description of job and what I did there and stuff yadda yadda yaddya yadda hdahhha", 
+        footer: "Start Date - End Date"
+    }
+]
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,28 @@
 import { Section } from "../../Components/section/section"
+import { Card } from "../../Components/card/card"
+import { EDUCATION_CARD_DATA } from "../../Components/data/data"
+import { WORK_CARD_DATA } from "../../Components/data/data"
+import { PROJECT_CARD_DATA } from "../../Components/data/data"
 
 export function Home() {
-    const schoolInfo = <>
-        <h2>University of Nevada Reno</h2>
-        <p>Bachlors of Science in Information Systems</p>
-        <p>May 17, 2024</p>
-        </>
+    function convertToCard(input) {
+        return <Card heading={input.heading}
+                        subTitle={input.subHeading}
+                        content={input.content}
+                        footer={input.footer}
+        />
+    }
     return(
         <>
-            <Section title='Education' content={schoolInfo}/>
+            <Section title='Education' content={
+                EDUCATION_CARD_DATA.map (convertToCard)
+            }/>
+            <Section title='Work Expirance' content={
+                WORK_CARD_DATA.map (convertToCard)
+            }/>
+            <Section title='Special Projects' content={
+                PROJECT_CARD_DATA.map (convertToCard)
+            }/>
         </>
     )
         
