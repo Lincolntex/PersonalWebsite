7caa26e86ba4b5a52c0246243c70f5029cadfeb1
Lincolntex <chasen.m.young@gmail.com>
2024-02-22 19:16:35 -0800

    set up basic layout for about page. Fixes #4

---
+++ src/Components/aboutHero/aboutHero.jsx
@@ -0,0 +1,15 @@
+import './aboutHero.css'
+
+export function AboutHero () {
+    return (
+        <div className='a-hero-holder'>
+            <div className='image-frame'>
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-person-icon.png"></img>
+            </div>
+            <h2 className='title'>Chasen Young</h2>
+            <h4 className='desc'>Who is Chasen? Here are some hobbies and interests Chasen ejoys during his free time.</h4>
+            <div className='section-break'></div>
+
+        </div>
+    )
+}
--- src/Components/homeHero/homeHero.jsx
+++ src/Components/homeHero/homeHero.jsx
@@ -1,10 +1,10 @@
 import './homeHero.css'
 
-export function HomeHero (props) {
+export function HomeHero () {
     return (
         <div className='h-hero-holder'>
             <div className='image-frame'>
-                <img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-person-icon.png"></img>
+                <img src="https://i.pinimg.com/736x/de/59/4e/de594ec09881da3fa66d98384a3c72ff.jpg"></img>
             </div>
             <h2 className='title'>Chasen Young</h2>
             <h4 className='desc'>A graduate of the Univeristy of Nevada, Reno with a BS in Information Systems working toward a career in Web Development.</h4>
